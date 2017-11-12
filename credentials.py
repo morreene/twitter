60ac5a79aa9f5b9bd7aca97380a26bd99519bff7
morreene <morreene@gmail.com>
2017-11-12 22:12:45 +0100

    new commit

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'pW70aIQpd4WeepQuFXgUwt7T5'
+CONSUMER_SECRET = 'CFATCxyZrSeCoKtBuNkARmHLKViMBKysTQGuVgXEccI3G1gn3m'
+
+# Access:
+ACCESS_TOKEN  = '19938974-OXGGFVHqTrm6vXGV5eqpWE2JK089x38v4wkMTElQx'
+ACCESS_SECRET = 'alXzszbzNyJzZi9ZHR6KaXO55QFUMSu8nG8P8xVKgsjzS'
